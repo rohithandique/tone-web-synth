feaa4055d6734338c2575da7e78b2407c2744bcf
Rohit Handique <rohand.181cv135@nitk.edu.in>
2020-10-28 15:58:01 +0530

    fixed octave slider string bug

--- public/index.js
+++ public/index.js
@@ -5,6 +5,7 @@ const synth = new Tone.Synth().toDestination();
 
 /*setting the number of octaves to be 3.
 then adding the major and minor keys for each octave*/
+/*
 let keys = "";
 let currentOctave = 1;
 for (let octave = currentOctave; octave < currentOctave + 3; octave++) {
@@ -25,15 +26,42 @@ for (let octave = currentOctave; octave < currentOctave + 3; octave++) {
     keys += `<h6 class="keyName">${note + (octave)}</h6></div>`;
   }
 }
-
+*/
 /*adding it to the html body*/
-document.getElementById('piano').innerHTML = keys;
+//document.getElementById('piano').innerHTML = keys;
+
+function octaveSlider(value) {
+  document.getElementById('piano').innerHTML = "";
+  document.getElementById('octaveValue').innerHTML = value;
+  let currentOctave = parseInt(value);
+  let newKeys = "";
+
+  for (let octave = currentOctave; octave < currentOctave + 3; octave++) {
+    for (let i = 0; i < notes.length; i++) {
+      let hasSharp = true;
+      let note = notes[i];
+      if (note == 'E' || note == 'B') {
+        hasSharp = false;
+      }
+      newKeys += `<div class='majorKey'
+      data-note='${note + (octave)}'>`;
+      if (hasSharp) {
+        newKeys += `<div class='minorKey'
+        data-note='${note + '#' + (octave)}'>
+        <h6 class="keyName ">${note + '#' + (octave)}</h6>
+        </div>`;
+      }
+      newKeys += `<h6 class="keyName">${note + (octave)}</h6></div>`;
+    }
+  }
+  document.getElementById('piano').innerHTML = newKeys;
+  addingListeners();
+}
 
 function addingListeners() {
-  /*selecting the major and minor key*/
   const majorKeyElem = document.querySelectorAll('.majorKey');
   const minorKeyElem = document.querySelectorAll('.minorKey');
-
+  
   /*triggering the note for mousepress*/
   majorKeyElem.forEach(item => {
     item.addEventListener('mousedown', event => {
@@ -43,7 +71,7 @@ function addingListeners() {
       item.lastChild.style.background = '#ccc';
     })
   })
-
+  
   /*releasing the note when the mouse leaves*/
   majorKeyElem.forEach(item => {
     item.addEventListener('mouseup', event => {
@@ -53,7 +81,7 @@ function addingListeners() {
       item.lastChild.style.background = 'white';
     })
   })
-
+  
   /*triggering the note for mousepress*/
   minorKeyElem.forEach(item => {
     item.addEventListener('mousedown', event => {
@@ -63,7 +91,7 @@ function addingListeners() {
       item.style.background = 'black';
     })
   })
-
+  
   /*releasing the note when the mouse leaves*/
   minorKeyElem.forEach(item => {
     item.addEventListener('mouseup', event => {
@@ -73,12 +101,12 @@ function addingListeners() {
       item.style.background = '#333';
     })
   })
-
+  
   /*getting the key pressed through the keyboard.
     S  D    G   H  J 
   Z  X  C  V  B  N  M 
   this is what an octave should look like*/
-
+  
   let pressedKey = "";
   document.addEventListener("keypress", event => {
     switch (event.key) {
@@ -150,45 +178,19 @@ function addingListeners() {
         document.querySelector(`[data-note=${pressedKey}]`).lastChild.style.background = '#ccc';
         break;
     }
-
+  
     /*triggering the corresponding sound when the correct key is pressed*/
     if (["z", "Z", "s", "S", "x", "X", "d", "D", "c", "C", "v", "V", "g", "G", "b", "B", "h", "H", "n", "N", "j", "J", "m", "M"]
       .includes(event.key)) {
       synth.triggerAttack(pressedKey, '16n');
     }
-
-  })
-
-  /*releasing the sound when key is not pressed anymore*/
-  document.addEventListener("keyup", event => {
-    synth.triggerRelease();
   })
 }
-addingListeners();
-function octaveSlider(value) {
-  document.getElementById('octaveValue').innerHTML = value;
-  currentOctave = value;
-  let newKeys = "";
-  document.getElementById('piano').innerHTML = "";
 
-  for (let octave = currentOctave; octave < currentOctave + 3; octave++) {
-    for (let i = 0; i < notes.length; i++) {
-      let hasSharp = true;
-      let note = notes[i];
-      if (note == 'E' || note == 'B') {
-        hasSharp = false;
-      }
-      newKeys += `<div class='majorKey'
-      data-note='${note + (octave)}'>`;
-      if (hasSharp) {
-        newKeys += `<div class='minorKey'
-        data-note='${note + '#' + (octave)}'>
-        <h6 class="keyName ">${note + '#' + (octave)}</h6>
-        </div>`;
-      }
-      newKeys += `<h6 class="keyName">${note + (octave)}</h6></div>`;
-    }
-  }
-  document.getElementById('piano').innerHTML = newKeys;
-  addingListeners();
-}
+
+
+/*releasing the sound when key is not pressed anymore*/
+document.addEventListener("keyup", event => {
+  synth.triggerRelease();
+})
+
